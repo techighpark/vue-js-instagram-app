663958fda24c3347e404d632af210a55aba87787
techighpark <techigh.b@gmail.com>
2022-09-08 17:03:14 +0900

    layout

---
+++ src/components/Container.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <div v-for="(post, i) in posts" :key="i">
+    <Post :post="post" />
+  </div>
+</template>
+<script>
+import Post from '@/components/Post.vue';
+export default {
+  components: {
+    Post,
+  },
+  props: {
+    posts: Array,
+  },
+};
+</script>
+<style lang=""></style>
---
+++ src/components/Post.vue
@@ -0,0 +1,68 @@
+<template lang="">
+  <div class="post">
+    <div class="post-header">
+      <div class="profile" :style="profileCssObject"></div>
+      <span class="profile-name">{{ post.name }}</span>
+    </div>
+    <div class="post-body" :style="postCssObject"></div>
+    <div class="post-content">
+      <p>{{ post.likes }}</p>
+      <p>
+        <strong>{{ post.name }}</strong> {{ post.content }}
+      </p>
+      <p class="date">{{ post.date }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'post',
+  props: {
+    post: Object,
+  },
+  data() {
+    return {
+      profileCssObject: { backgroundImage: `url(${this.post.userImage})` },
+      postCssObject: { backgroundImage: `url(${this.post.postImage})` },
+    };
+  },
+};
+</script>
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+}
+</style>
