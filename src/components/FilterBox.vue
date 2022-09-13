41f21e177e5d9f684640ea475ff4454dbc1477fd
techighpark <techigh.b@gmail.com>
2022-09-13 14:18:27 +0900

    vuex

---
+++ src/components/FilterBox.vue
@@ -0,0 +1,30 @@
+<template lang="">
+  <div @click="fire" class="filter-item" :class="filter" :style="image">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    image: Object,
+    filter: String,
+  },
+  methods: {
+    fire() {
+      this.emitter.emit('clickFilter', this.filter);
+    },
+  },
+};
+</script>
+<style>
+.filter-item {
+  width: 100px;
+  height: 100px;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color: white;
+  background-size: cover;
+  background-position: center;
+}
+</style>
